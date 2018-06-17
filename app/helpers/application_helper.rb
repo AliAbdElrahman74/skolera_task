ecf390ed460b491f3b468e4e5d7ec2045cb2c883
Alexandre Angelim <angelim@angelim.com.br>
2018-06-17 13:06:19 +0100

    Fixes styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def nav_link(name, path)
+    link_to name, path, class: "#{request.path.starts_with?(path) ? 'active' : ''}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 [
   { number: '+12345678910',  enabled: true, nickname: "US number" },
   { number: '+441915556666', enabled: true, nickname: "UK number" },
@@ -31,15 +23,12 @@ end
   end
 end
 
+CALLS_PER_AGENT = 100
+
 phone_numbers = PhoneNumber.all
 agents = Agent.all
 
-phone_numbers.each do |phone_number|
-  agents.each do |agent|
-    PhoneCall.create!(phone_number: phone_number, agent: agent, status: 'ended')
-  end
+agents.each do |agent|
+  CALLS_PER_AGENT.times { agent.phone_calls.create!(phone_number: phone_numbers.sample, status: 'ended') }
+  agent.phone_calls.create!(phone_number: phone_numbers.sample, status: 'in_progress')
 end
-
-agent = Agent.find_by(name: 'Chaminda')
-
-PhoneCall.create!(phone_number: phone_numbers.last, agent: agent, status: 'in_progress')
